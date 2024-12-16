ba4012d8f9390930401609472c7495015c16d0a8
syyjbhjh3 <syyjbhjh7@gmail.com>
2024-12-16 16:51:37 +0900

    [User] Insert : prettier Config Inject

--- login-app/src/components/calendar/MiniCalendar.tsx
+++ login-app/src/components/calendar/MiniCalendar.tsx
@@ -9,37 +9,41 @@ import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import Card from 'components/card/Card';
 type CalendarValue = Date | [Date, Date] | null;
 export default function MiniCalendar(props: {
-  selectRange: boolean;
-  [x: string]: any;
+    selectRange: boolean;
+    [x: string]: any;
 }) {
-  const { selectRange, ...rest } = props;
-  const [value, setValue] = useState<CalendarValue>(new Date());
-  const handleChange = (
-    value: CalendarValue,
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-  ) => {
-    setValue(value);
-    alert('New date is: ' + value);
-  };
-  return (
-    <Card
-      alignItems="center"
-      flexDirection="column"
-      w="100%"
-      maxW="max-content"
-      p="20px 15px"
-      h="max-content"
-      {...rest}
-    >
-      <Calendar
-        onChange={handleChange}
-        value={value}
-        selectRange={selectRange}
-        view={'month'}
-        tileContent={<Text color="brand.500" />}
-        prevLabel={<Icon as={MdChevronLeft} w="24px" h="24px" mt="4px" />}
-        nextLabel={<Icon as={MdChevronRight} w="24px" h="24px" mt="4px" />}
-      />
-    </Card>
-  );
+    const { selectRange, ...rest } = props;
+    const [value, setValue] = useState<CalendarValue>(new Date());
+    const handleChange = (
+        value: CalendarValue,
+        event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    ) => {
+        setValue(value);
+        alert('New date is: ' + value);
+    };
+    return (
+        <Card
+            alignItems="center"
+            flexDirection="column"
+            w="100%"
+            maxW="max-content"
+            p="20px 15px"
+            h="max-content"
+            {...rest}
+        >
+            <Calendar
+                onChange={handleChange}
+                value={value}
+                selectRange={selectRange}
+                view={'month'}
+                tileContent={<Text color="brand.500" />}
+                prevLabel={
+                    <Icon as={MdChevronLeft} w="24px" h="24px" mt="4px" />
+                }
+                nextLabel={
+                    <Icon as={MdChevronRight} w="24px" h="24px" mt="4px" />
+                }
+            />
+        </Card>
+    );
 }
--- login-app/src/views/auth/signUp/index.tsx
+++ login-app/src/views/auth/signUp/index.tsx
@@ -1,196 +1,185 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-// Chakra imports
+import React, { useState } from 'react';
 import {
-  Box,
-  Button,
-  Checkbox,
-  Flex,
-  FormControl,
-  FormLabel,
-  Heading,
-  Icon,
-  Input,
-  InputGroup,
-  InputRightElement,
-  Text,
-  useColorModeValue,
-} from "@chakra-ui/react";
-// Custom components
-import { HSeparator } from "components/separator/Separator";
-import DefaultAuth from "layouts/auth/Default";
-// Assets
-import illustration from "assets/img/auth/auth.png";
-import { FcGoogle } from "react-icons/fc";
-import { MdOutlineRemoveRedEye } from "react-icons/md";
-import { RiEyeCloseLine } from "react-icons/ri";
+    Box,
+    Button,
+    Flex,
+    FormControl,
+    FormLabel,
+    Heading,
+    Icon,
+    Input,
+    InputGroup,
+    InputRightElement,
+    Text,
+    useColorModeValue,
+} from '@chakra-ui/react';
+import axios, { AxiosError } from 'axios'; // axios 추가
+import DefaultAuth from 'layouts/auth/Default';
+import illustration from 'assets/img/auth/auth.png';
+import { MdOutlineRemoveRedEye } from 'react-icons/md';
+import { RiEyeCloseLine } from 'react-icons/ri';
 
 function SignUp() {
-  // Chakra color mode
-  const textColor = useColorModeValue("navy.700", "white");
-  const textColorSecondary = "gray.400";
-  const textColorDetails = useColorModeValue("navy.700", "secondaryGray.600");
-  const textColorBrand = useColorModeValue("brand.500", "white");
-  const brandStars = useColorModeValue("brand.500", "brand.400");
-  const googleBg = useColorModeValue("secondaryGray.300", "whiteAlpha.200");
-  const googleText = useColorModeValue("navy.700", "white");
-  const googleHover = useColorModeValue(
-    { bg: "gray.200" },
-    { bg: "whiteAlpha.300" }
-  );
-  const googleActive = useColorModeValue(
-    { bg: "secondaryGray.300" },
-    { bg: "whiteAlpha.200" }
-  );
-  const [show, setShow] = React.useState(false);
-  const handleClick = () => setShow(!show);
-  return (
-    <DefaultAuth illustrationBackground={illustration} image={illustration}>
-      <Flex
-        maxW={{ base: "100%", md: "max-content" }}
-        w='100%'
-        mx={{ base: "auto", lg: "0px" }}
-        me='auto'
-        h='100%'
-        alignItems='start'
-        justifyContent='center'
-        mb={{ base: "30px", md: "60px" }}
-        px={{ base: "25px", md: "0px" }}
-        mt={{ base: "40px", md: "14vh" }}
-        flexDirection='column'>
-        <Box me='auto'>
-          <Heading color={textColor} fontSize='36px' mb='10px'>
-            Sign Up
-          </Heading>
-          <Text
-            mb='36px'
-            ms='4px'
-            color={textColorSecondary}
-            fontWeight='400'
-            fontSize='md'>
-            Sign up and Usage our Platform!
-          </Text>
-        </Box>
-        <Flex
-          zIndex='2'
-          direction='column'
-          w={{ base: "100%", md: "420px" }}
-          maxW='100%'
-          background='transparent'
-          borderRadius='15px'
-          mx={{ base: "auto", lg: "unset" }}
-          me='auto'
-          mb={{ base: "20px", md: "auto" }}>
+    // State 관리
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [name, setName] = useState('');
+    const [show, setShow] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-          <FormControl>
-            <FormLabel
-              display='flex'
-              ms='4px'
-              fontSize='sm'
-              fontWeight='500'
-              color={textColor}
-              mb='8px'>
-              Email<Text color={brandStars}>*</Text>
-            </FormLabel>
-            <Input
-              isRequired={true}
-              variant='auth'
-              fontSize='sm'
-              ms={{ base: "0px", md: "0px" }}
-              type='email'
-              placeholder='mail@simmmple.com'
-              mb='24px'
-              fontWeight='500'
-              size='lg'
-            />
-            <FormLabel
-              ms='4px'
-              fontSize='sm'
-              fontWeight='500'
-              color={textColor}
-              display='flex'>
-              Password<Text color={brandStars}>*</Text>
-            </FormLabel>
-            <InputGroup size='md'>
-              <Input
-                isRequired={true}
-                fontSize='sm'
-                placeholder='New Password'
-                mb='24px'
-                size='lg'
-                type={show ? "text" : "password"}
-                variant='auth'
-              />
-              <InputRightElement display='flex' alignItems='center' mt='4px'>
-                <Icon
-                  color={textColorSecondary}
-                  _hover={{ cursor: "pointer" }}
-                  as={show ? RiEyeCloseLine : MdOutlineRemoveRedEye}
-                  onClick={handleClick}
-                />
-              </InputRightElement>
-            </InputGroup>
-            <FormLabel
-              ms='4px'
-              fontSize='sm'
-              fontWeight='500'
-              color={textColor}
-              display='flex'>
-              Confirm Password<Text color={brandStars}>*</Text>
-            </FormLabel>
-            <InputGroup size='md'>
-              <Input
-                  isRequired={true}
-                  fontSize='sm'
-                  placeholder='Confirm Password'
-                  mb='24px'
-                  size='lg'
-                  type={show ? "text" : "password"}
-                  variant='auth'
-              />
-              <InputRightElement display='flex' alignItems='center' mt='4px'>
-                <Icon
-                    color={textColorSecondary}
-                    _hover={{ cursor: "pointer" }}
-                    as={show ? RiEyeCloseLine : MdOutlineRemoveRedEye}
-                    onClick={handleClick}
-                />
-              </InputRightElement>
-            </InputGroup>
-            <FormLabel
-                display='flex'
-                ms='4px'
-                fontSize='sm'
-                fontWeight='500'
-                color={textColor}
-                mb='8px'>
-              Name<Text color={brandStars}>*</Text>
-            </FormLabel>
-            <Input
-                isRequired={true}
-                variant='auth'
-                fontSize='sm'
-                ms={{ base: "0px", md: "0px" }}
-                type='email'
-                placeholder='Your Name'
-                mb='24px'
-                fontWeight='500'
-                size='lg'
-            />
-            <Button
-              fontSize='sm'
-              variant='brand'
-              fontWeight='500'
-              w='100%'
-              h='50'
-              mb='24px'>
-              Sign Up
-            </Button>
-          </FormControl>
-        </Flex>
-      </Flex>
-    </DefaultAuth>
-  );
+    const handleClick = () => setShow(!show);
+
+    // API 호출 로직
+    const handleSignUp = async () => {
+        if (password !== confirmPassword) {
+            alert('Passwords do Not match!');
+            return;
+        }
+
+        try {
+            const response = await axios.post(
+                'https://api.example.com/signup',
+                {
+                    /* Type  */
+                    email,
+                    password,
+                    name,
+                },
+            );
+            console.log('Sign Up Success:', response.data);
+        } catch (error) {
+            if (error instanceof AxiosError) {
+                console.error(
+                    'Sign Up Error:',
+                    error.response?.data || error.message,
+                );
+            } else {
+                console.error('Unexpected Error:', error);
+            }
+        }
+    };
+
+    // Chakra UI Color Mode
+    const textColor = useColorModeValue('navy.700', 'white');
+    const textColorSecondary = 'gray.400';
+    const brandStars = useColorModeValue('brand.500', 'brand.400');
+
+    return (
+        <DefaultAuth illustrationBackground={illustration} image={illustration}>
+            <Flex
+                maxW={{ base: '100%', md: 'max-content' }}
+                w="100%"
+                mx={{ base: 'auto', lg: '0px' }}
+                me="auto"
+                h="100%"
+                alignItems="start"
+                justifyContent="center"
+                mb={{ base: '30px', md: '60px' }}
+                px={{ base: '25px', md: '0px' }}
+                mt={{ base: '40px', md: '14vh' }}
+                flexDirection="column"
+            >
+                <Box me="auto">
+                    <Heading color={textColor} fontSize="36px" mb="10px">
+                        Sign Up
+                    </Heading>
+                    <Text
+                        mb="36px"
+                        ms="4px"
+                        color={textColorSecondary}
+                        fontWeight="400"
+                        fontSize="md"
+                    >
+                        Sign up and Usage our Platform!
+                    </Text>
+                </Box>
+                <Flex
+                    zIndex="2"
+                    direction="column"
+                    w={{ base: '100%', md: '420px' }}
+                    maxW="100%"
+                    background="transparent"
+                    borderRadius="15px"
+                    mx={{ base: 'auto', lg: 'unset' }}
+                    me="auto"
+                    mb={{ base: '20px', md: 'auto' }}
+                >
+                    <FormControl>
+                        <FormLabel color={textColor}>
+                            Email<Text color={brandStars}>*</Text>
+                        </FormLabel>
+                        <Input
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            type="email"
+                            placeholder="mail@simmmple.com"
+                            mb="24px"
+                        />
+
+                        <FormLabel color={textColor}>
+                            Password<Text color={brandStars}>*</Text>
+                        </FormLabel>
+                        <InputGroup size="md">
+                            <Input
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                type={show ? 'text' : 'password'}
+                                placeholder="New Password"
+                                mb="24px"
+                            />
+                            <InputRightElement>
+                                <Icon
+                                    as={
+                                        show
+                                            ? RiEyeCloseLine
+                                            : MdOutlineRemoveRedEye
+                                    }
+                                    onClick={handleClick}
+                                />
+                            </InputRightElement>
+                        </InputGroup>
+
+                        <FormLabel color={textColor}>
+                            Confirm Password<Text color={brandStars}>*</Text>
+                        </FormLabel>
+                        <InputGroup size="md">
+                            <Input
+                                value={confirmPassword}
+                                onChange={(e) =>
+                                    setConfirmPassword(e.target.value)
+                                }
+                                type={show ? 'text' : 'password'}
+                                placeholder="Confirm Password"
+                                mb="24px"
+                            />
+                        </InputGroup>
+
+                        <FormLabel color={textColor}>
+                            Name<Text color={brandStars}>*</Text>
+                        </FormLabel>
+                        <Input
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            type="text"
+                            placeholder="Your Name"
+                            mb="24px"
+                        />
+
+                        <Button
+                            isLoading={loading}
+                            onClick={handleSignUp}
+                            colorScheme="teal"
+                            w="100%"
+                            h="50px"
+                        >
+                            Sign Up
+                        </Button>
+                    </FormControl>
+                </Flex>
+            </Flex>
+        </DefaultAuth>
+    );
 }
 
 export default SignUp;
