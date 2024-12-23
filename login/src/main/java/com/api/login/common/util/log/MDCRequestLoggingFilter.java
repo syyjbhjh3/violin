40c9f7e6f61ae8e31b3f26b75c86dd007d78c923
syyjbhjh3 <syyjbhjh7@gmail.com>
2024-12-23 16:36:27 +0900

    :memo: [login] Update : Micrometer Update

--- login/src/main/java/com/api/login/common/util/log/MDCRequestLoggingFilter.java
+++ login/src/main/java/com/api/login/common/util/log/MDCRequestLoggingFilter.java
@@ -2,29 +2,30 @@ package com.api.login.common.util.log;
 
 import jakarta.servlet.*;
 import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.UUID;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class MDCRequestLoggingFilter implements Filter {
 
+    @Value("${spring.application.name}")
+    private String appName;
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
 
-//        String uuid = UUID.randomUUID().toString();
-//        MDC.put("trace_id", uuid);
-//        MDC.put("resource", "Auth");
-//
-//        try {
-//            chain.doFilter(request, response);
-//        } finally {
-//            MDC.clear();
-//        }
+        MDC.put("application", appName);
+
+        try {
+            chain.doFilter(request, response);
+        } finally {
+            MDC.clear();
+        }
     }
 }
