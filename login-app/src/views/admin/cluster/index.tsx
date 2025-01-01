e74d4cbfb10a87613287b4b5a5a4dde869f04c68
parkyoungjun <syyjbhjh7@naver.com>
2025-01-01 23:32:12 +0900

    :memo: [login-app] Update : UI Clear

--- login-app/src/views/admin/cluster/index.tsx
+++ login-app/src/views/admin/cluster/index.tsx
@@ -25,7 +25,6 @@ import React from 'react';
 // Chakra imports
 import {
     Box,
-    Button,
     Flex,
     Grid,
     Text,
@@ -36,8 +35,6 @@ import {
 
 // Custom components
 import Banner from './components/Banner';
-import TableTopCreators from './components/TableTopCreators';
-import HistoryItem from './components/HistoryItem';
 import NFT from 'components/card/NFT';
 import Card from 'components/card/Card';
 import RegistrationPopup from './popup/ClusterRegist';
@@ -47,15 +44,12 @@ import RegistrationPopup from './popup/ClusterRegist';
 import Nft1 from 'assets/img/nfts/Nft1.png';
 import Nft2 from 'assets/img/nfts/Nft2.png';
 import Nft3 from 'assets/img/nfts/Nft3.png';
-import Nft4 from 'assets/img/nfts/Nft4.png';
-import Nft5 from 'assets/img/nfts/Nft5.png';
-import Nft6 from 'assets/img/nfts/Nft6.png';
-import tableDataTopCreators from './variables/tableDataTopCreators';
+import ComplexTable from "../default/components/ComplexTable";
+import tableDataComplex from "../default/variables/tableDataComplex";
 
 export default function Cluster() {
-    // Chakra Color Mode
     const textColor = useColorModeValue('secondaryGray.900', 'white');
-    const textColorBrand = useColorModeValue('brand.500', 'white');
+    const tableTitle = 'Cluster Status';
 
     const { isOpen, onOpen, onClose } = useDisclosure();
 
@@ -132,122 +126,18 @@ export default function Cluster() {
                                 btn="Service"
                             />
                         </SimpleGrid>
-                        <Text
-                            mt="45px"
-                            mb="36px"
-                            color={textColor}
-                            fontSize="2xl"
-                            ms="24px"
-                            fontWeight="700"
-                        >
-                            Recently Added
-                        </Text>
-                        <SimpleGrid
-                            columns={{ base: 1, md: 3 }}
-                            gap="20px"
-                            mb={{ base: '20px', xl: '0px' }}
-                        >
-                            <NFT
-                                name="Swipe Circles"
-                                author="By Peter Will"
-                                bidders={[
-                                ]}
-                                image={Nft4}
-                                currentbid="0.91 ETH"
-                                download="#"
-                                btn="123"
-                            />
-                            <NFT
-                                name="Colorful Heaven"
-                                author="By Mark Benjamin"
-                                bidders={[
-                                ]}
-                                image={Nft5}
-                                currentbid="0.91 ETH"
-                                download="#"
-                                btn="123"
-                            />
-                            <NFT
-                                name="3D Cubes Art"
-                                author="By Manny Gates"
-                                bidders={[
-                                ]}
-                                image={Nft6}
-                                currentbid="0.91 ETH"
-                                download="#"
-                                btn="123"
-                            />
-                        </SimpleGrid>
                     </Flex>
                 </Flex>
                 <Flex
                     flexDirection="column"
                     gridArea={{ xl: '1 / 3 / 2 / 4', '2xl': '1 / 2 / 2 / 3' }}
                 >
-                    <Card px="0px" mb="20px">
-                        <TableTopCreators tableData={tableDataTopCreators} />
+                    <Card p="0px" mb="20px">
+                        <ComplexTable tableData={tableDataComplex} tableTitle={tableTitle} />
                     </Card>
-                    <Card p="0px">
-                        <Flex
-                            align={{ sm: 'flex-start', lg: 'center' }}
-                            justify="space-between"
-                            w="100%"
-                            px="22px"
-                            py="18px"
-                        >
-                            <Text
-                                color={textColor}
-                                fontSize="xl"
-                                fontWeight="600"
-                            >
-                                History
-                            </Text>
-                            <Button variant="action">See all</Button>
-                        </Flex>
-
-                        <HistoryItem
-                            name="Colorful Heaven"
-                            author="By Mark Benjamin"
-                            date="30s ago"
-                            image={Nft5}
-                            price="0.91 ETH"
-                        />
-                        <HistoryItem
-                            name="Abstract Colors"
-                            author="By Esthera Jackson"
-                            date="58s ago"
-                            image={Nft1}
-                            price="0.91 ETH"
-                        />
-                        <HistoryItem
-                            name="ETH AI Brain"
-                            author="By Nick Wilson"
-                            date="1m ago"
-                            image={Nft2}
-                            price="0.91 ETH"
-                        />
-                        <HistoryItem
-                            name="Swipe Circles"
-                            author="By Peter Will"
-                            date="1m ago"
-                            image={Nft4}
-                            price="0.91 ETH"
-                        />
-                        <HistoryItem
-                            name="Mesh Gradients "
-                            author="By Will Smith"
-                            date="2m ago"
-                            image={Nft3}
-                            price="0.91 ETH"
-                        />
-                        <HistoryItem
-                            name="3D Cubes Art"
-                            author="By Manny Gates"
-                            date="3m ago"
-                            image={Nft6}
-                            price="0.91 ETH"
-                        />
-                    </Card>
+                    {/*<Card px="0px">*/}
+                    {/*    <TableTopCreators tableData={tableDataTopCreators} />*/}
+                    {/*</Card>*/}
                 </Flex>
             </Grid>
             {/* Delete Product */}
