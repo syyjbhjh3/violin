4014b1a91d409654f86b9496a8e07d2a8f784189
parkyoungjun <syyjbhjh7@naver.com>
2025-02-19 21:59:56 +0900

    :memo: [User] Insert : Eureka Service Regist

--- user/src/main/java/com/api/login/LoginApplication.java
+++ user/src/main/java/com/api/login/LoginApplication.java
@@ -1,11 +1,14 @@
 package com.api.login;
 
+import jdk.jfr.Enabled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableDiscoveryClient
 @EnableCaching
+@SpringBootApplication
 public class LoginApplication {
 
 	public static void main(String[] args) {
