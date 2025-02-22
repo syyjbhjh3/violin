4c8567882dc91eda7a1d8b535c3bc56251c91a18
parkyoungjun <syyjbhjh7@naver.com>
2025-02-22 23:58:28 +0900

    :memo: [Gateway, User] Insert : User Route Test, Cors Filter, Logging Global FIlter

---
+++ gateway/src/main/java/com/api/gateway/filter/web/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.api.gateway.filter.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
